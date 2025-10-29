5c62d122a0be1b3adfeb18a8c9c4003bdd0e33d3
simor <it.simone.belfiore@gmai.com>
2025-10-29 13:48:15 +0100

    Add API Integration Test

---
+++ BondEvaluator.API.IntegrationTest/BondEvaluatorControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace BondEvaluator.API.IntegrationTest;
+
+public class BondEvaluatorControllerTest
+{
+    private const string CorrectCsvTetsPath = "./TestData/bond_positions_sample.csv";
+    private const string CorruptedCsvTetsPath = "./TestData/bond_positions_sample_corrupted.csv";
+    
+    [Fact]
+    public async Task WhenFileIsCorrect_ThenReturnsResult()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>();
+        var client = factory.CreateClient();
+        var file = await File.ReadAllTextAsync(CorrectCsvTetsPath);
+        var content = new MultipartFormDataContent();
+        var csvBytes = Encoding.UTF8.GetBytes(file);
+        var fileContent = new ByteArrayContent(csvBytes);
+        fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("text/csv");
+        content.Add(fileContent, "file", "test.csv");
+
+        // Act
+        var response = await client.PostAsync("/bondevaluator/bondevaluations", content);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.Equal("text/csv", response.Content.Headers.ContentType?.MediaType);
+    }
+    
+    [Fact]
+    public async Task WhenFileIsCorrect_ThenReturnsBadRequest()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>();
+        var client = factory.CreateClient();
+        var file = await File.ReadAllTextAsync(CorruptedCsvTetsPath);
+        var content = new MultipartFormDataContent();
+        var csvBytes = Encoding.UTF8.GetBytes(file);
+        var fileContent = new ByteArrayContent(csvBytes);
+        fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("text/csv");
+        content.Add(fileContent, "file", "test.csv");
+
+        // Act
+        var response = await client.PostAsync("/bondevaluator/bondevaluations", content);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+}
--- BondEvaluator.API/Program.cs
+++ BondEvaluator.API/Program.cs
@@ -6,41 +6,49 @@ using BondEvaluator.Application.Services;
 using BondEvaluator.Infrastructure.DependencyInjection;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace BondEvaluator.API;
 
-// Add services to the container.
-
-builder.Services.AddRouting(options=>options.LowercaseUrls = true);
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-// Configure logger
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Infinite)
-    .CreateLogger();
-builder.Host.UseSerilog();
-
-builder.Services.AddScoped<IBondEvaluatorService, BondEvaluatorService>();
-builder.Services.AddSingleton<IRateParser, RateParser>();
-builder.Services.RegisterExternalServices();
-
-builder.Services.Configure<BondEvaluatorConfiguration>(
-    builder.Configuration.GetSection(BondEvaluatorConfiguration.Section));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.MapOpenApi();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddRouting(options=>options.LowercaseUrls = true);
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        // Configure logger
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Infinite)
+            .CreateLogger();
+        builder.Host.UseSerilog();
+
+        builder.Services.AddScoped<IBondEvaluatorService, BondEvaluatorService>();
+        builder.Services.AddSingleton<IRateParser, RateParser>();
+        builder.Services.RegisterExternalServices();
+
+        builder.Services.Configure<BondEvaluatorConfiguration>(
+            builder.Configuration.GetSection(BondEvaluatorConfiguration.Section));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+        app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "Swagger"));
+        app.UseHttpsRedirection();
+
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "Swagger"));
-app.UseHttpsRedirection();
-
-app.UseMiddleware<ExceptionHandlingMiddleware>();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
